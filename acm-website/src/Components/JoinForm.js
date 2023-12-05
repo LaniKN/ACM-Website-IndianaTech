7a93bcc7d2a65537f6a9971bad4663ff564257cc
LaniKN <nohealani2022@gmail.com>
2023-12-05 11:54:43 -0500

    Added small details to form submission

--- acm-website/src/Components/JoinForm.js
+++ acm-website/src/Components/JoinForm.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react';
+import {useNavigate} from 'react-router-dom';
 import '../pages/BecomeMember.css';
-import axios from 'axios';
 
 
 
@@ -8,21 +8,30 @@ const JoinForm = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("")
     const [prefName, setPrefName] = useState("")
-    const [email, setemail] = useState("");
+    const [email, setEmail] = useState("");
     const [setYear] = useState("");
     const [major, setMajor] = useState("");
     const [setGender] = useState("");
     // const scriptUrl = "https://script.google.com/macros/s/AKfycbw4OgSGYNRQsM1V3EKRu64yDKhr2aSRfyMSL-RJ-YMs5r9hd_GiFodGibmuo9qXiUI30g/exec"
     const scriptUrl = "https://sheetdb.io/api/v1/rqf1o6d61x43t"
     const formRef = useRef(null)
+    const navigate = useNavigate();
+    
 
 
     const handleSubmit = (e) =>{
         e.preventDefault()
+        setFirstName("")
+        setLastName("")
+        setPrefName("")
+        setEmail("")
+        setMajor("")
+        navigate('/events')
 
         fetch(scriptUrl, {method: 'POST', mode:'no-cors', body: new FormData(formRef.current)})
         .then(res => {
             console.log("SUCCESSFULLY SUBMITTED")
+            alert("Thank you for your submission!")
         })
         .catch(err => console.log(err))
     }
@@ -55,7 +64,7 @@ const JoinForm = () => {
                 <div className="question">
                     <label htmlFor='Email'>
                         Indiana Tech Email Address:
-                        <input type="email" className="txtResponse" name="Email" placeholder="myEmail01@indianatech.net" value={email} onChange={(a) => setemail(a.target.value)} required
+                        <input type="email" className="txtResponse" name="Email" placeholder="myEmail01@indianatech.net" value={email} onChange={(a) => setEmail(a.target.value)} required
                         />
                     </label>
                 </div>
